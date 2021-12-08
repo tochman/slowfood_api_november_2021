5707d5d4bd7538dd117d8bf87a6fdfe3823b3d47
Thomas <thomas@craftacademy.se>
2021-12-08 10:41:11 +0100

    adds spec to test if order id is part of the response

--- spec/requests/api/orders/create_spec.rb
+++ spec/requests/api/orders/create_spec.rb
@@ -2,6 +2,8 @@
 # 2. We need a User so we know who ordered a Product
 # 3. We need to be able to create an Order and connect/associate it with the User
 # 4. We need to associate the Order with a Product using a join table that we will call OrderItem
+# 5. Ideally the response should include the order and order items, so that this info can be used on the client side
+#    (the minuimum is to iclude the order id in the response)
 
 RSpec.describe 'POST /api/orders', type: :request do
   let(:user) { create(:user) }
@@ -32,5 +34,10 @@ RSpec.describe 'POST /api/orders', type: :request do
 
   it 'is expected to associate the product with the order' do
     expect(@order.products.first).to eq product
+  end
+
+  it 'is expected to include the ID of the order in the response body' do
+    response_body = JSON.parse(response.body)
+    expect(response_body['order']['id']).to eq @order.id
   end
 end
